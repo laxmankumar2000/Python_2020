df5a2befb5ed8b0ac8019a5b43e8ffed49fc9b78
Laxman Kumar <59361284+laxmankumar2000@users.noreply.github.com>
2020-05-15 15:20:41 +0530

    Hollow_Triangular_Pattern

---
+++ Tringular_Pattern.py
@@ -0,0 +1,19 @@
+'''
+         Hellow Trianle
+
+'''
+
+
+
+h = int(input('Enter the no. of lines.'))
+p = 0
+for i in range(1, h+1):
+       print(' '*(h-i),end = '')
+       while p != 2*i-1:
+           if p == 0 or p == 2*i-2 or i == h:
+                print('*',end='')
+           else:
+                print(' ',end='')
+           p += 1
+       print()
+       p = 0
